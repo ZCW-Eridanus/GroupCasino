356fea00751f107a36b4dd1b34a46553145b1142
tb0902 <tbruton417@gmail.com>
2024-03-21 08:02:55 -0400

    All Deck, Hand, and Card classes are complete. Each has Tests except for Hand. -Trey

---
+++ src/main/java/com/github/zipcodewilmington/casino/games/blackjack/BlackJack.java
@@ -0,0 +1,37 @@
+package com.github.zipcodewilmington.casino.games.blackjack;
+
+import com.github.zipcodewilmington.casino.CasinoAccount;
+import com.github.zipcodewilmington.casino.GameInterface;
+
+public class BlackJack implements GameInterface  {
+
+  @Override
+  public void run() {
+
+  }
+
+  @Override
+  public void run(CasinoAccount acct) {
+
+  }
+
+  @Override
+  public void playGame() {
+
+  }
+
+  @Override
+  public void setWager(int wager) {
+
+  }
+
+  @Override
+  public void action() {
+
+  }
+
+  @Override
+  public void exitGame() {
+
+  }
+}
